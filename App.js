54869ba558e598333888ea97bd87ad9ee8462d9f
kasperonfire <breindal70@gmail.com>
2017-10-26 10:26:56 +0200

    whatever

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Text, View, Platform, TouchableOpacity, StyleSheet, Button, WebView, Sc
 import { Constants, WebBrowser } from "expo";
 import { StackNavigator } from 'react-navigation';
 import Basics from "./JS/Basics";
+import WhatToDo from "./JS/WhatToDo";
 import Props from "./JS/Props";
 import State from "./JS/State";
 import Styles from "./JS/Styles";
@@ -19,24 +20,7 @@ const Touchable = (props) => (
     <Text style={styles.buttonText}>{props.title}</Text>
   </TouchableOpacity>)
 
-//Todo: Refactor into a seperate file (WhatToDo.js), import and complete the exercise
-class WhatToDo extends React.Component {
-  static navigationOptions = { title: "What I have to do" }
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <Text style={{ marginBottom: 3 }}>Complete all steps in Facebooks "The Basics" tutorial (see below)</Text>
-        <Text style={{ marginBottom: 3 }}>For each of the 11 steps, add a corresponding Component in this project + the necessecary changes to navigate into it.
-        (inspired of how you navigated into this)
-      </Text>
-        <WebView
-          source={{ uri: "https://facebook.github.io/react-native/docs/tutorial.html" }}
-          style={{ marginTop: 20, flex: 1 }}
-        />
-      </View>
-    );
-  }
-}
+
 
 class HomeScreen extends React.Component {
   static navigationOptions = { title: 'Day1 Tutorial' };
