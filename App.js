bab7d4a950aab69a9d845e74f2ab7c295c10cc36
kasperonfire <breindal70@gmail.com>
2017-11-03 12:13:38 +0100

    added working app

--- App.js
+++ App.js
@@ -3,17 +3,7 @@ import { Text, View, Platform, TouchableOpacity, StyleSheet, Button, WebView, Sc
 import { Constants, WebBrowser } from "expo";
 import { StackNavigator } from 'react-navigation';
 import Basics from "./JS/Basics";
-import WhatToDo from "./JS/WhatToDo";
-import Props from "./JS/Props";
-import State from "./JS/State";
-import Styles from "./JS/Styles";
-import HeightWidth from "./JS/HeightWidth";
-import FlexBox from "./JS/FlexBox";
-import TextInput from "./JS/TextInput";
-import Touches from "./JS/Touches";
-import ViewScroll from "./JS/ViewScroll";
-import ListView from "./JS/ListViews";
-import Networking from "./JS/Networking";
+
 
 const Touchable = (props) => (
   <TouchableOpacity style={styles.button} onPress={props.onPress}>
@@ -31,18 +21,7 @@ class HomeScreen extends React.Component {
         <Text style={{ textAlign: "center", fontSize: 20 }}>See all Demos implemented by</Text>
         <Text style={{ textAlign: "center", fontSize: 20 }}>Kasper Ravn Breindal</Text>
         <Text style={{ textAlign: "center", fontSize: 16 }}>http://github.com/kasperonfire/tutorial-native-app</Text>
-        <Touchable onPress={() => navigate('web')} title="What I have to do" />
         <Touchable onPress={() => navigate('basics')} title="Basics" />
-        <Touchable onPress={() => navigate('props')} title="Props" />
-        <Touchable onPress={() => navigate('state')} title="State" />
-		<Touchable onPress={() => navigate('styles')} title="Styles" />
-        <Touchable onPress={() => navigate('heightwidth')} title="HeightWidth" />
-		<Touchable onPress={() => navigate('flexbox')} title="FlexBox" />
-		<Touchable onPress={() => navigate('textinput')} title="TextInput" />
-		<Touchable onPress={() => navigate('Touches')} title="Touches" />
-		<Touchable onPress={() => navigate('ViewScroll')} title="ViewScroll" />
-        <Touchable onPress={() => navigate('listview')} title="ListView" />
-        <Touchable onPress={() => navigate('networking')} title="Networking" />
       </ScrollView>
     )
   }
@@ -52,18 +31,7 @@ export default App = () => <RouteStack style={{ marginTop: Platform.OS === 'ios'
 
 const RouteStack = StackNavigator({
   Home: { screen: HomeScreen },
-  basics: { screen: Basics },
-  props: { screen: Props },
-  web: { screen: WhatToDo },
-  state: { screen: State },
-  styles: { screen: Styles },
-  heightwidth: { screen: HeightWidth },
-  flexbox: { screen: FlexBox },
-  listview: { screen: ListView },
-  ViewScroll: { screen: ViewScroll },
-  networking: { screen: Networking },
-  textinput: {screen: TextInput },
-  Touches: {screen: Touches }
+  basics: { screen: Basics }
 });
 
 const styles = StyleSheet.create({
